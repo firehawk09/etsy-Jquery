82a1ca4991660a807f2d4527b1ea90f4d44bc515
Rafael Quijada <RQuijada@SHR.Global>
2016-10-12 13:36:02 -0500

    hey whats up hello

--- js/app.js
+++ js/app.js
@@ -3,30 +3,17 @@ window.onload = app;
 // runs when the DOM is loaded
 function app(){
     "use strict";
+    
+    document.querySelector("html").style.opacity = 1;
+    // start app?
+    var key = "hvo4ybhgni81tihncd8wlhv0",
+    secret = "donf2ympyi";
 
-    // load some scripts (uses promises :D)
-    loader.load(
-        //css
-        {url: "./dist/style.css"},
-        //js
-        {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/lodash.min.js"},
-        {url: "./bower_components/backbone/backbone.js"},
-        {url: "./js/etsy.js"}
-    ).then(function(){
-        document.querySelector("html").style.opacity = 1;
-        // start app?
-        var key = "hvo4ybhgni81tihncd8wlhv0",
-        secret = "donf2ympyi";
-
-        $("form").on("submit", function(event){
-            event.preventDefault();
-            window.location.hash = '#/search/'+this.querySelector('input').value;
-        })
-
-
-        new EtsyClient(key);
+    $("form").on("submit", function(event){
+        event.preventDefault();
+        window.location.hash = '#/search/'+this.querySelector('input').value;
     })
 
 
+    new EtsyClient(key);
 }
